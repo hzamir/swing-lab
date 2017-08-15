2d1367a7c32a00e8a8227cc8eff5d63ea5cf72a1
Haim Zamir <hzamir@gmail.com>
2017-08-14 21:48:12 -0400

    validation + make jar executable

--- src/main/java/com/baliset/conf/SwingLabDefaultsConfig.java
+++ src/main/java/com/baliset/conf/SwingLabDefaultsConfig.java
@@ -3,11 +3,7 @@ package com.baliset.conf;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-//
-//import javax.validation.constraints.Max;
-//import javax.validation.constraints.Min;
-//import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Configuration
 @ConfigurationProperties(prefix = "app.defaults")
@@ -15,11 +11,11 @@ import org.springframework.context.annotation.Configuration;
 public class SwingLabDefaultsConfig implements InitializingBean
 {
 
-  public int initialRows;
-  public int initialSectors;
+  @Min(1)           public int initialRows;
+  @Min(1) @Max(300) public int initialSectors;
 
-  public void setInitialRows(int initialRows)       { this.initialRows = initialRows; }
-  public void setInitialSectors(int initialSectors) { this.initialSectors = initialSectors; }
+  public void setInitialRows(int v)    { initialRows    = v; }
+  public void setInitialSectors(int v) { initialSectors = v; }
 
   @Override
   public void afterPropertiesSet() throws Exception
